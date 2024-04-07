c09b8c5c5a2d4723e75d81ce1532be5234a15234
arijitmajumdar-IISc <151856679+arijitmajumdar-IISc@users.noreply.github.com>
2024-04-08 04:59:31 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 import pickle
 import numpy as np
 
@@ -9,12 +9,7 @@ y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-# Initialize the Random Forest Classifier
-model = RandomForestClassifier(n_estimators=100, random_state=42)
+model = LogisticRegression().fit(X, y)
 
-# Fit the model to the data
-model.fit(X, y)
-
-# Save the trained model to a file
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
